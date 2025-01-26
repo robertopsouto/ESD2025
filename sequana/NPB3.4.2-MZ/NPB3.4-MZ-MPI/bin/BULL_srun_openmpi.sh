f1fd72be61ddb981ba59d7bd465abcc458a1c78f
rpsouto <rpsouto@lncc.br>
2025-01-26 16:04:51 -0300

    modified:   sequana/NPB3.4.2-MZ/NPB3.4-MZ-MPI/bin/BULL_srun_openmpi.sh

--- sequana/NPB3.4.2-MZ/NPB3.4-MZ-MPI/bin/BULL_srun_openmpi.sh
+++ sequana/NPB3.4.2-MZ/NPB3.4-MZ-MPI/bin/BULL_srun_openmpi.sh
@@ -3,11 +3,10 @@
 #SBATCH --nodes=1                       # here the number of nodes
 #SBATCH --ntasks=1                      # here total number of mpi tasks
 #SBATCH --cpus-per-task=1               # number of cores per node
-#SBATCH -p sequana_cpu_dev                      # target partition
+#SBATCH -p sequana_cpu_dev              # target partition
 #SBATCH --threads-per-core=1
 #SBATCH -J NPB_BT-MZ                    # job name
 #SBATCH --time=00:05:00                 # time limit
-#SBATCH --exclusive                     # to have exclusive use of your nodes
 
 echo "Cluster configuration:"
 echo "==="
@@ -16,8 +15,11 @@ echo "Number of nodes: " $SLURM_NNODES
 echo "Number of MPI processes: " $SLURM_NTASKS " (" $SLURM_NNODES " nodes)"
 echo "Number of MPI processes per node: " $SLURM_NTASKS_PER_NODE
 echo "Number of threads per MPI process: " $SLURM_CPUS_PER_TASK
+echo "Job node list:"
+srun hostname
 echo "NPB Benchmark: " $1
 echo "Bechmark class problem: " $2
+
 
 ###################################
 #           COMPILER              #
